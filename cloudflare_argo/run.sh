106926f3f7ba7d252745ee5213c49286da44cc29
Phongsakorn Wisetthon <boyphongsakorn@users.noreply.github.com>
2021-12-31 10:29:08 +0700

    Update run.sh

--- cloudflare_argo/run.sh
+++ cloudflare_argo/run.sh
@@ -4,6 +4,7 @@ mkdir -p /etc/cloudflared
 
 echo $(bashio::config 'credential_file_content') > /etc/cloudflared/credentials.json
 echo $(bashio::config 'config_file_content_base64') | base64 -d > /etc/cloudflared/config.yml
+echo $(bashio::config 'config_file_content') > /etc/cloudflared/config.yml
 
 bashio::log.info "Running cloudflared update..."
 
